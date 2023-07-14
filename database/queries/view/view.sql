afe4bd40e61a6417423bcb7746d523ff050d9e20
RickaPrincy <rckprincy@gmail.com>
2023-07-14 14:35:44 +0300

    feat: add some view : total price per reservation + total money paid per reservation

--- database/queries/view/view.sql
+++ database/queries/view/view.sql
@@ -1,3 +1,6 @@
+--All necessary views
+
+-- view to get all reservation_id which is active for the current moment
 CREATE VIEW "reservation_actual" AS 
 (
     SELECT "reservation"."id_reservation"
@@ -5,4 +8,23 @@ CREATE VIEW "reservation_actual" AS
     WHERE "reservation"."is_annulled" = false
     AND "reservation"."arrival" <= CURRENT_TIMESTAMP
     AND "reservation"."departure" >= CURRENT_TIMESTAMP
+);
+
+-- view to get total price of reservation 
+CREATE VIEW "reservation_price_total" AS 
+(
+    SELECT "reservation_contain"."id_reservation", SUM("room"."price_per_night" * "reservation_contain"."quantity") as "total_price"
+    FROM "reservation_contain" 
+    INNER JOIN "room" 
+    ON "reservation_contain"."id_room" = "room"."id_room"
+    GROUP BY "reservation_contain"."id_reservation"
+);
+
+-- view to get total money paid per reservation 
+CREATE VIEW "reservation_price_paid" AS (
+    SELECT "reservation"."id_reservation", SUM("payment"."amount_paid") as "total_paid"
+    FROM "reservation" 
+    INNER JOIN "payment" 
+    ON "payment"."id_reservation" = "reservation"."id_reservation"
+    GROUP BY "reservation"."id_reservation"
 );
