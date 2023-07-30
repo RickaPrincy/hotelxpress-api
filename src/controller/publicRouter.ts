12e06a1f0fbea844396604009b0190eead3e647b
RickaPrincy <rckprincy@gmail.com>
2023-07-30 15:27:42 +0300

    add getRoomContentIlike

--- src/controller/publicRouter.ts
+++ src/controller/publicRouter.ts
@@ -6,16 +6,19 @@ import { getUserById } from "../services/user/getUser";
 import { getAllRooms, getRoomById, getRoomByIntervalAndLocation } from "../services/room/getRoom";
 import { deleteRoom } from "../services/room/roomCrud";
 import { getAllHotels, getHotelById } from "../services/hotel/getHotel";
+import { getAllRoomContent, getRoomContentIlike } from "../services/room/getRoomContent";
 
 export const publicRouter = Router();
 
 //Some endpoint are temporary public
 publicRouter.delete("/delete/user/:id",deleteUser);
 publicRouter.get("/find/room/interval", getRoomByIntervalAndLocation);
+publicRouter.get("/find/room_content/:content_name", getRoomContentIlike);
 publicRouter.get("/user/:id",getUserById);
 publicRouter.get("/room/:id",getRoomById);
 publicRouter.get("/delete/room/:id",deleteRoom);
 publicRouter.get("/hotels",getAllHotels);
+publicRouter.get("/room_contents",getAllRoomContent);
 publicRouter.get("/hotel/:id", getHotelById);
 
 publicRouter.get("/rooms", getAllRooms);
